3f23ec51ca35e4127f15c829733887908e68a79f
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-10-21 09:13:56 +0200

    si

--- EjemplosAnimaciones/app/src/main/java/com/example/ejemplosanimaciones/traslasion.java
+++ EjemplosAnimaciones/app/src/main/java/com/example/ejemplosanimaciones/traslasion.java
@@ -6,15 +6,17 @@ import android.view.animation.AnimationUtils;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class traslasion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-
+        Animation miAnimacion = AnimationUtils.loadAnimation(this, R.anim.traslacion);
+        miAnimacion.setRepeatMode(Animation.RESTART);
+        miAnimacion.setRepeatCount(20);
     }
 }
