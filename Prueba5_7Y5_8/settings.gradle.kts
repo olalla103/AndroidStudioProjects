fa8157cc8ececfda04d063fa553e1a88ad7ac6be
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-10-25 11:55:22 +0200

    prueba

---
+++ Prueba5_7Y5_8/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prueba5_7Y5_8"
+include(":app")
+ 
