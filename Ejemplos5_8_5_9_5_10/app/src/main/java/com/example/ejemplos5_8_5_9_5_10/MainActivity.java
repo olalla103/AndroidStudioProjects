469bc1936f394b68c683424e5ba24700f7021cf9
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-10-30 12:34:40 +0100

    si

--- Ejemplos5_8_5_9_5_10/app/src/main/java/com/example/ejemplos5_8_5_9_5_10/MainActivity.java
+++ Ejemplos5_8_5_9_5_10/app/src/main/java/com/example/ejemplos5_8_5_9_5_10/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ejemplos5_8_5_9_5_10;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // EJEMPLO 5.7
+        String[] opciones = {"Opción 1", "Opción 2", "Opción 3", "Opción 4", "Opción 5"};
+        AutoCompleteTextView textoLeido = (AutoCompleteTextView) findViewById(R.id.autoComplete);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, opciones);
+        textoLeido.setAdapter(adaptador);
     }
 }
