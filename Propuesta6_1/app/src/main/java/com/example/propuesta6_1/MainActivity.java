f74a727b3f726d07030573c1547746c4c87ead68
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-11-11 09:21:21 +0100

    cumple de Pao

--- Propuesta6_1/app/src/main/java/com/example/propuesta6_1/MainActivity.java
+++ Propuesta6_1/app/src/main/java/com/example/propuesta6_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.propuesta6_1;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ListView lista = findViewById(R.id.lista);
+        final String[] valoresLista = {"España", "Alemania", "Lituania", "Croacia", "Italia", "Eslovenia", "Dinamarca", "República Checa"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, valoresLista);
+        lista.setAdapter(adaptador);
     }
 }
