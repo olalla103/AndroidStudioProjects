a74f20663f524aec7dd8cd131fad445fddbb90ec
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-11-11 19:12:01 +0100

    11 noviembre

---
+++ Propuesta6_1/app/src/main/java/com/example/propuesta6_1/AniadeTexto.java
@@ -0,0 +1,24 @@
+package com.example.propuesta6_1;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AniadeTexto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_aniade_texto);
+
+        String palabra;
+        EditText texto = findViewById(R.id.editTextText);
+        palabra = texto.getText().toString().trim();
+    }
+}
--- Propuesta6_1/app/src/main/java/com/example/propuesta6_1/MainActivity.java
+++ Propuesta6_1/app/src/main/java/com/example/propuesta6_1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.propuesta6_1;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +25,35 @@ public class MainActivity extends AppCompatActivity {
         final String[] valoresLista = {"España", "Alemania", "Lituania", "Croacia", "Italia", "Eslovenia", "Dinamarca", "República Checa"};
         ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, valoresLista);
         lista.setAdapter(adaptador);
+        Button aniadir = findViewById(R.id.aniadir);
+        Button eliminar = findViewById(R.id.eliminar);
+
+        aniadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Código para el botón "eliminar"
+                Toast.makeText(getApplicationContext(), "Eliminar pulsado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    public static void aniadir(String[] valoresLista, String palabra) {
+        valoresLista = Arrays.copyOf(valoresLista, valoresLista.length + 1);
+        valoresLista[valoresLista.length - 1] = palabra;
+
+    }
+
+    public static void eliminar(String[] valoresLista) {
+        valoresLista = Arrays.copyOf(valoresLista, valoresLista.length - 1);
+
     }
 }
