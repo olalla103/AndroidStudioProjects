e51377739a9474e117a310f3fac60b5da75c39e1
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-11-25 23:01:25 +0100

    trabajo

--- AppGestion/app/src/main/java/com/example/appgestion/ListaElementos.java
+++ AppGestion/app/src/main/java/com/example/appgestion/ListaElementos.java
@@ -9,9 +9,6 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
@@ -26,17 +23,18 @@ public class ListaElementos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lista_elementos);
+
         lista = (ListView) findViewById(R.id.lista);
         texto = (TextView) findViewById(R.id.texto);
-        ArrayList<MainActivity.Encapsulador> datos = new ArrayList<MainActivity.Encapsulador>();
+        ArrayList<Encapsulador> datos = new ArrayList<>();
 
         // tengo que cambiar las imágenes
         datos.add(new ListaElementos.Encapsulador(R.drawable.camisetanegra, "Camiseta negra", "Camiseta negra de manga larga y tejido de canalé (textura acanalada). Tiene un escote redondeado y amplio, con un pequeño detalle de lazo decorativo en la parte superior del centro. Es ajustada al cuerpo.", true));
-        datos.add(new MainActivity.Encapsulador(R.drawable.vestidolentejuelas, "Vestido lentejuelas", "El vestido está decorado completamente con lentejuelas multicolor que forman un patrón brillante en tonos rosados, dorados y plateados. Es un vestido midi con un corte ajustado y detalles distintivos: el escote es cruzado en la parte superior, dejando una pequeña abertura en el centro del pecho, y tiene un diseño halter con tiras que se atan en el cuello. ", false));
-        datos.add(new MainActivity.Encapsulador(R.drawable.bolsomarron, "Bolso marrón cuero", "Diseño tipo hobo, amplio y funcional. Está en cuero de color marrón oscuro, con un acabado liso y elegante. Cuenta con dos cremalleras decorativas en la parte frontal que añaden un toque moderno", false));
-        datos.add(new MainActivity.Encapsulador(R.drawable.vaqueroballoon, "Vaquero Balloon", "Corte relajado tipo \"balloon\" con cinco bolsillos, costuras torcidas y cierre de cremallera con botón metálico. Su diseño combina comodidad y estilo, con un fit amplio y cintura de tiro medio", false));
-        datos.add(new MainActivity.Encapsulador(R.drawable.botasojales, "Botín ojales", "Diseño moderno y audaz, con un tacón alto estilizado y ojales metálicos decorativos que añaden un toque de vanguardia", false));
-        datos.add(new MainActivity.Encapsulador(R.drawable.faldaplisadapicos, "Falda plisada picos", "Con un diseño de estilo midi con un corte asimétrico que presenta un bajo en picos, lo que añade un toque moderno y fluido a su apariencia. Está confeccionada en un tejido ligero y suave que permite un movimiento elegante, ideal para combinar con prendas casuales o más formales", false));
+        datos.add(new ListaElementos.Encapsulador(R.drawable.vestidolentejuelas, "Vestido lentejuelas", "El vestido está decorado completamente con lentejuelas multicolor que forman un patrón brillante en tonos rosados, dorados y plateados. Es un vestido midi con un corte ajustado y detalles distintivos: el escote es cruzado en la parte superior, dejando una pequeña abertura en el centro del pecho, y tiene un diseño halter con tiras que se atan en el cuello. ", false));
+        datos.add(new ListaElementos.Encapsulador(R.drawable.bolsomarron, "Bolso marrón cuero", "Diseño tipo hobo, amplio y funcional. Está en cuero de color marrón oscuro, con un acabado liso y elegante. Cuenta con dos cremalleras decorativas en la parte frontal que añaden un toque moderno", false));
+        datos.add(new ListaElementos.Encapsulador(R.drawable.vaqueroballoon, "Vaquero Balloon", "Corte relajado tipo \"balloon\" con cinco bolsillos, costuras torcidas y cierre de cremallera con botón metálico. Su diseño combina comodidad y estilo, con un fit amplio y cintura de tiro medio", false));
+        datos.add(new ListaElementos.Encapsulador(R.drawable.botasojales, "Botín ojales", "Diseño moderno y audaz, con un tacón alto estilizado y ojales metálicos decorativos que añaden un toque de vanguardia", false));
+        datos.add(new ListaElementos.Encapsulador(R.drawable.faldaplisadapicos, "Falda plisada picos", "Con un diseño de estilo midi con un corte asimétrico que presenta un bajo en picos, lo que añade un toque moderno y fluido a su apariencia. Está confeccionada en un tejido ligero y suave que permite un movimiento elegante, ideal para combinar con prendas casuales o más formales", false));
 
 
         lista.setAdapter(new Adaptador(this, R.layout.entrada, datos) {
@@ -74,6 +72,7 @@ public class ListaElementos extends AppCompatActivity {
             }
         });
     }
+
     class Encapsulador {
         private int imagen;
         private String titulo, texto;
