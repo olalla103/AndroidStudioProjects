d160f52cc7afe3d9e5da6a85b3b7768e8a8e2ea7
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-12-02 09:15:00 +0100

    añadir elementos, terminar método

--- AppGestion/app/src/main/java/com/example/appgestion/ListaElementos.java
+++ AppGestion/app/src/main/java/com/example/appgestion/ListaElementos.java
@@ -1,6 +1,8 @@
 package com.example.appgestion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -32,7 +34,7 @@ public class ListaElementos extends AppCompatActivity {
 
         // tengo que cambiar las imágenes
         datos.add(new ListaElementos.Encapsulador(R.drawable.camisetanegra_mujer, "Camiseta negra", true));
-        datos.add(new ListaElementos.Encapsulador(R.drawable.vestidolentejuelasMujer, "Vestido lentejuelas", false));
+        datos.add(new ListaElementos.Encapsulador(R.drawable.vestidolentejuelas_mujer, "Vestido lentejuelas", false));
         datos.add(new ListaElementos.Encapsulador(R.drawable.bolsomarron, "Bolso marrón cuero", false));
         datos.add(new ListaElementos.Encapsulador(R.drawable.vaqueroballoon_mujer, "Vaquero Balloon", false));
         datos.add(new ListaElementos.Encapsulador(R.drawable.botasojales_mujer, "Botín ojales", false));
@@ -51,23 +53,6 @@ public class ListaElementos extends AppCompatActivity {
                     // Configurar los datos
                     texto_superior_entrada.setText(encapsulador.get_textoTitulo());
                     imagen_entrada.setImageResource(encapsulador.get_idImagen());
-
-//                    // Configurar el listener del RadioButton para mostrar la descripción en lugar de "MARCADA UNA OPCION"
-//                    miRadio.setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View v) {
-//                            // Desmarcar el RadioButton anterior si existe
-//                            if (radioButton_pulsado != null) radioButton_pulsado.setChecked(false);
-//
-//                            // Marcar el nuevo RadioButton y guardarlo como el seleccionado
-//                            radioButton_pulsado = miRadio;
-//                            radioButton_pulsado.setChecked(true); // Asegurar que el actual se marque
-//
-//                            // Mostrar la descripción del elemento seleccionado
-//                            texto.setText(encapsulador.get_textoContenido());
-//                        }
-//                    });
-
                 }
             }
         });
@@ -82,15 +67,38 @@ public class ListaElementos extends AppCompatActivity {
         }
     }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            // Acción al presionar el icono (puedes abrir un DrawerLayout o mostrar un mensaje)
-            Toast.makeText(this, "Menú hamburguesa pulsado", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+
+    // Inflar menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.popup_menu, menu);
+        return true;
     }
 
+    // Al seleccionar añadir y eliminar en el menú
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.aniadir) {
+            // Acción para añadir un elemento
+            // Intent para ir a la otra actividad
+            Intent intent = new Intent(this, AniadirElemento.class);
+            startActivity(intent);
+            Toast.makeText(this, "Añadiendo elemento", Toast.LENGTH_SHORT).show();
+            // Lógica para añadir un elemento a la lista
+            lista.setAdapter(new Adaptador(this, R.layout.entrada, datos)); // tengo que coger los elementos de la otra clase
+
+            return true;
+        } else if (item.getItemId() == R.id.eliminar) {
+            // Acción para eliminar un elemento
+            Toast.makeText(this, "Eliminando elemento", Toast.LENGTH_SHORT).show();
+            // Lógica para eliminar un elemento de la lista
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // Clase encapsuladora
     class Encapsulador {
         private int imagen;
         private String titulo, texto;
